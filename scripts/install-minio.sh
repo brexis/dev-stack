af3675b08c0df7f82b786dd035f0b87a52af9b19
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-06 00:11:15 +0100

    Fix

--- scripts/install-minio.sh
+++ scripts/install-minio.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+ACCESS_KEY=$1
+SECRET_KEY=$2
+
 if [ -f "$HOME/.minio" ]
 then
-    echo "Minio already installed."
-    exit 0
+  echo "Minio already installed."
+  exit 0
 fi
 
 touch "$HOME/.minio"
@@ -16,14 +19,15 @@ sudo useradd -r minio-user -s /sbin/nologin
 sudo mkdir /usr/local/share/minio
 sudo mkdir /etc/minio
 
-sudo cat <<EOT >> /etc/default/minio
+block="
 # Local export path.
-MINIO_VOLUMES="/usr/local/share/minio/"
+MINIO_VOLUMES=\"/usr/local/share/minio/\"
 # Use if you want to run Minio on a custom port.
-MINIO_OPTS="--config-dir /etc/minio --address :9600"
-MINIO_ACCESS_KEY=homestead
-MINIO_SECRET_KEY=secretkey
-EOT
+MINIO_OPTS=\"--config-dir /etc/minio --address :9600\"
+MINIO_ACCESS_KEY=$ACCESS_KEY
+MINIO_SECRET_KEY=$SECRET_KEY
+"
+sudo echo "$block" > /etc/default/minio
 
 sudo chown minio-user:minio-user /usr/local/share/minio
 sudo chown minio-user:minio-user /etc/minio
@@ -40,4 +44,4 @@ sudo ufw allow 9600
 curl -sO https://dl.minio.io/client/mc/release/linux-amd64/mc
 chmod +x mc
 sudo mv mc /usr/local/bin
-mc config host add homestead http://127.0.1.1:9600 homestead secretkey
+mc config host add minio http://127.0.1.1:9600 $ACCESS_KEY $SECRET_KEY
