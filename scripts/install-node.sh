972aa254f5a9c0778de63c07c63904ebb5d65432
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-05 22:22:11 +0100

    fix nvm installation

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -3,6 +3,4 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 echo -e "export NVM_DIR=\"\$HOME/.nvm\"\n [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"\n [ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"" | tee -a "$HOME/.zshrc"
-zsh
-nvm install 8
-nvm use 8
+/bin/zsh -i -c  "nvm install 8 && nvm use 8"
--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -9,7 +9,7 @@ sudo mv phpbrew /usr/local/bin/phpbrew
 phpbrew init
 
 echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" | tee -a "$HOME/.zshrc"
-zsh
+
 echo "Install Php 7.2 7.1 7.0 5.6"
 phpbrew install 7.2.13 +default +dbs +fpm +gd
 phpbrew install 7.1.25 +default +dbs +fpm +gd
