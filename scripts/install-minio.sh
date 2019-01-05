7ebd211cd18b98d7b3182cec05fc0f4b6aa05e1d
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-06 00:27:04 +0100

    Fix

--- scripts/install-minio.sh
+++ scripts/install-minio.sh
@@ -27,8 +27,7 @@ MINIO_OPTS=\"--config-dir /etc/minio --address :9600\"
 MINIO_ACCESS_KEY=$ACCESS_KEY
 MINIO_SECRET_KEY=$SECRET_KEY
 "
-sudo touch /etc/default/minio
-sudo echo "$block" > /etc/default/minio
+echo "$block" > sudo tee /etc/default/minio
 
 sudo chown minio-user:minio-user /usr/local/share/minio
 sudo chown minio-user:minio-user /etc/minio
