1188c8518be1c605c10a4e2ba3bcac35918b1690
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-05 18:39:08 +0100

    fix mysl installation

--- scripts/install-mysql.sh
+++ scripts/install-mysql.sh
@@ -6,7 +6,7 @@ STACK_DIR="$HOME/.stack"
 ROOT_PASSWORD_PATH="$STACK_DIR/MYSQL_ROOT_PASSWORD"
 ROOT_PASSWORD=$(openssl rand -hex 8)
 
-if [ -z $STACK_DIR ]; then
+if [ ! -d $STACK_DIR ]; then
   mkdir -p $STACK_DIR
 fi
 
@@ -17,15 +17,14 @@ echo "$ROOT_PASSWORD" | tee "$ROOT_PASSWORD_PATH"
 
 echo "Install MySql server"
 
-sudo apt update
-sudo apt install -y mysql-server mysql-client libmysqlclient-dev libmysqld-dev
-
 # Set The Automated Root Password
 
 sudo debconf-set-selections <<< "mysql-server mysql-server/data-dir select ''"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $ROOT_PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $ROOT_PASSWORD"
 
+sudo apt update
+sudo apt install -y mysql-server mysql-client libmysqlclient-dev libmysqld-dev
 sudo service mysql restart
 
 mysql --user="root" --password="$ROOT_PASSWORD" -e "CREATE USER '$USERNAME'@'localhost' IDENTIFIED BY '$PASSWORD';"
