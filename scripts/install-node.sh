7a8384c84f637b1d1f585a93c6aa6115d8e11f8a
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-05 22:05:49 +0100

    fix

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -2,15 +2,7 @@
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-if [ -e "$HOME/.zshrc" ]; then
-  SOURCE_FILE="$HOME/.zshrc"
-else
-  SOURCE_FILE="$HOME/.bashrc"
-fi
-
-NVM_EXPORT="export NVM_DIR=\"\$HOME/.nvm\"\n [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"\n [ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\""
-echo -e "$NVM_EXPORT" | tee -a "$SOURCE_FILE"
-eval $NVM_EXPORT
-
+echo -e "export NVM_DIR=\"\$HOME/.nvm\"\n [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"\n [ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"" | tee -a "$HOME/.zshrc"
+zsh
 nvm install 8
 nvm use 8
--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -8,14 +8,8 @@ sudo mv phpbrew /usr/local/bin/phpbrew
 
 phpbrew init
 
-if [ -e "$HOME/.zshrc" ]; then
-  SOURCE_FILE="$HOME/.zshrc"
-else
-  SOURCE_FILE="$HOME/.bashrc"
-fi
-
-echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" | tee -a $SOURCE_FILE
-
+echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" | tee -a "$HOME/.zshrc"
+zsh
 echo "Install Php 7.2 7.1 7.0 5.6"
 phpbrew install 7.2.13 +default +dbs +fpm +gd
 phpbrew install 7.1.25 +default +dbs +fpm +gd
