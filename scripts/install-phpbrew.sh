f7ff294e6b474fb307ccc0860e32dfdb3b7ce788
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-06 16:09:21 +0100

    Add install php script

--- scripts/install-phpbrew.sh
+++ scripts/install-phpbrew.sh
@@ -9,6 +9,7 @@ sudo mv phpbrew /usr/local/bin/phpbrew
 # Init phpbrew
 phpbrew init
 [[ -e "$HOME/.phpbrew/bashrc" ]] && source "$HOME/.phpbrew/bashrc"
+echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" | tee -a "$HOME/.bashrc"
 
 phpbrew app get composer
 phpbrew app get phpunit
