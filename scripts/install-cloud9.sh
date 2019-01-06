5980ceb6b38e468b71d34caf1e2d2d5c3e5252ff
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-06 01:09:14 +0100

    Add supervisor config file

--- scripts/install-cloud9.sh
+++ scripts/install-cloud9.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+HOST=$1
+USERNAME=$2
+PASSWORD=$3
+USER=$(whoami)
 INSTALL_DIR="$HOME/ide"
 WORKSPACE_DIR="$HOME/code"
 
@@ -16,3 +20,20 @@ fi
 git clone https://github.com/c9/core.git $INSTALL_DIR
 cd $INSTALL_DIR
 /bin/bash scripts/install-sdk.sh
+
+# Add supervisor task
+block="
+[program:cloud9]
+command=node $INSTALL_DIR/server.js --listen $HOST --port 80 -w $WORKSPACE_DIR -a $USERNAME:$PASSWORD
+user=$USER
+autostart=true
+autorestart=true
+stdout_logfile=/var/log/supervisor/cloud9.log
+stderr_logfile=/var/log/supervisor/cloud9_errors.log
+environment=NODE_ENV=\"production\",HOME="$WORKSPACE_DIR"
+"
+echo "$BLOCK" | tee /etc/supervisor/conf.d/cloud9.conf
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start cloud9:*
