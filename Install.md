f623a4dee916251dae19f61652597d85b92ac0e1
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-06 15:06:15 +0100

    add source

--- Install.md
+++ Install.md
@@ -1,3 +1,9 @@
+## Install Locales
+```
+locale
+sudo locale-gen "en_US.UTF-8"
+sudo dpkg-reconfigure locales
+```
 ## Add swap
 https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-16-04
 ## Setup Server
--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -3,7 +3,5 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 echo -e "export NVM_DIR=\"\$HOME/.nvm\"\n [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"\n [ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"" | tee -a "$HOME/.bashrc"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source $HOME/.bashrc
 nvm install 8 && nvm use 8
