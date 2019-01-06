3d437acfbf3737572e507a0f5d868952058fd692
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-06 01:12:56 +0100

    debug

--- scripts/install-cloud9.sh
+++ scripts/install-cloud9.sh
@@ -6,20 +6,20 @@ USER=$(whoami)
 INSTALL_DIR="$HOME/ide"
 WORKSPACE_DIR="$HOME/code"
 
-if [ ! -d "$WORKSPACE_DIR" ]
-then
-  mkdir $WORKSPACE_DIR
-fi
+# if [ ! -d "$WORKSPACE_DIR" ]
+# then
+#   mkdir $WORKSPACE_DIR
+# fi
 
-if [ -d "$INSTALL_DIR" ]
-then
-  echo "Cloud9 folder already exists."
-  exit 0
-fi
+# if [ -d "$INSTALL_DIR" ]
+# then
+#   echo "Cloud9 folder already exists."
+#   exit 0
+# fi
 
-git clone https://github.com/c9/core.git $INSTALL_DIR
-cd $INSTALL_DIR
-/bin/bash scripts/install-sdk.sh
+# git clone https://github.com/c9/core.git $INSTALL_DIR
+# cd $INSTALL_DIR
+# /bin/bash scripts/install-sdk.sh
 
 # Add supervisor task
 block="
