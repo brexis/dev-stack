3015139b4e33bc6c903a23a6d7b5178801f69b23
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-06 15:00:12 +0100

    source NVM source

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -2,5 +2,8 @@
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-echo -e "export NVM_DIR=\"\$HOME/.nvm\"\n [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"\n [ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"" | tee -a "$HOME/.bashrc"
+NVM_SOURCE="export NVM_DIR=\"\$HOME/.nvm\"\n [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"\n [ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\""
+echo -e "$NVM_SOURCE" | tee -a "$HOME/.bashrc"
+eval $NVM_SOURCE
+
 nvm install 8 && nvm use 8
