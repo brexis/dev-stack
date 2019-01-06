58b22719081f1bddd876bac56b2ed61cecdc6eda
Boris KOUMONDJI <brexis2009@yahoo.fr>
2019-01-06 15:19:15 +0100

    run nvm bash

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -2,6 +2,8 @@
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-source "$HOME/.bashrc"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 nvm install 8
 nvm use 8
