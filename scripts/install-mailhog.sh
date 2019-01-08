e50bc97c92794ed10893508e1746a5af7bccb2bf
Boris Koumondji <brexis2009@yahoo.fr>
2019-01-08 21:46:02 +0000

    Complete mailhog installation

---
+++ scripts/install-mailhog.sh
@@ -0,0 +1,18 @@
+# Install & Configure MailHog
+
+sudo wget --quiet -O /usr/local/bin/mailhog https://github.com/mailhog/MailHog/releases/download/v0.2.1/MailHog_linux_amd64
+sudo chmod +x /usr/local/bin/mailhog
+
+sudo tee /etc/systemd/system/mailhog.service <<EOL
+[Unit]
+Description=Mailhog
+After=network.target
+[Service]
+User=root
+ExecStart=/usr/bin/env /usr/local/bin/mailhog > /dev/null 2>&1 &
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo systemctl daemon-reload
+sudo systemctl enable mailhog
