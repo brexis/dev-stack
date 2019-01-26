cbeb474e1722107389e6544318ee086100d3f463
Boris Koumondji <brexis2009@yahoo.fr>
2019-01-26 13:28:07 +0000

    r/N/n

--- scripts/install-chrome.sh
+++ scripts/install-chrome.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# INstall Mate
+# Install Mate
 sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop -y
 sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon -y
