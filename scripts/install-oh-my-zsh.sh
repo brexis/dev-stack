e3e8a4824352c3f3ff39578ffe8ef0bbc5873571
Boris Koumondji <brexis2009@yahoo.fr>
2019-01-26 14:00:46 +0000

    Add bash_alias to zshrc

--- scripts/install-oh-my-zsh.sh
+++ scripts/install-oh-my-zsh.sh
@@ -3,3 +3,5 @@
 echo "Install Oh My Zsh"
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "if [ -f ~/.bash_aliases ]; then\n  . ~/.bash_aliases\nfi" | tee -a "$HOME/.zshrc"
