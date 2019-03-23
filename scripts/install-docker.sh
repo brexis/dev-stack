b08c149af32f21c2629a97ad9a869a485926a52a
Boris Koumondji <brexis2009@yahoo.fr>
2019-03-23 22:17:36 +0000

    Install docker compose

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -14,3 +14,7 @@ sudo add-apt-repository \
    stable"
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
