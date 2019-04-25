e1aae4a5c8e894bfb6b783206a30155ea0d9f31b
Boris Koumondji <brexis2009@yahoo.fr>
2019-04-25 11:48:25 +0000

    Fix zsh aliases

--- scripts/install-oh-my-zsh.sh
+++ scripts/install-oh-my-zsh.sh
@@ -4,4 +4,4 @@ echo "Install Oh My Zsh"
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "if [ -f ~/.bash_aliases ]; then\n  . ~/.bash_aliases\nfi" | tee -a "$HOME/.zshrc"
+echo -e "if [ -f ~/.bash_aliases ]; then\n  . ~/.bash_aliases\nfi" | tee -a "$HOME/.zshrc"
