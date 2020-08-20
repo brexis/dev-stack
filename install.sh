b0c21166714b6b2cfe5339b6aa0b28ca8d66ced7
Boris Koumondji <brexis2009@yahoo.fr>
2020-08-20 00:06:00 +0000

    Fix gpg keys issue

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ sudo apt install -y php  php-curl php-json php-cgi php-fpm autoconf \
 
 # Install Rvm
 echo "Installing rvm"
-gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
 \curl -sSL https://get.rvm.io | bash -s stable --rails
 
