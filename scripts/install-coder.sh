8552ea12e7333d35c31445bc0907ac5c304bedc4
Boris Koumondji <brexis2009@yahoo.fr>
2020-08-23 15:37:41 +0000

    Fix coder

--- scripts/install-coder.sh
+++ scripts/install-coder.sh
@@ -1,35 +1,30 @@
 #!/usr/bin/env bash
-USERNAME=$1
-PASSWORD=$2
 WORKSPACE_DIR="$HOME/code"
-VERSION="1.939-vsc1.33.1"
 
 if [ ! -d "$WORKSPACE_DIR" ]
 then
   mkdir $WORKSPACE_DIR
 fi
 
-cd $HOME
+$FILE="/etc/supervisor/conf.d/coder.conf"
 
-wget https://github.com/codercom/code-server/releases/download/$VERSION/code-server$VERSION-linux-x64.tar.gz
-tar -xvzf code-server$VERSION-linux-x64.tar.gz
-cd code-server$VERSION-linux-x64
-sudo chmod +x code-server
-sudo mv code-server /usr/local/bin/
-cd $HOME
-rm code-server$VERSION-linux-x64.tar.gz
-rm -R code-server$VERSION-linux-x64
+if grep -q "user=$USER" "$FILE"
+then
+  return
+fi
+
+curl -fsSL https://code-server.dev/install.sh | sh
 
 block="
 [program:coder]
-command=/usr/local/bin/code-server $HOME/code --port 8081 --password $PASSWORD
-user=root
+command=/usr/bin/code-server --config $HOME/.config/code-server/config.yaml $HOME/code
+user=$USER
 autostart=true
 autorestart=true
-stdout_logfile=/var/log/supervisor/coder.log
-stderr_logfile=/var/log/supervisor/coder_errors.log
+stdout_logfile=/var/log/supervisor/coder_$USER.log
+stderr_logfile=/var/log/supervisor/coder_errors_$USER.log
 "
-echo "$block" | sudo tee /etc/supervisor/conf.d/coder.conf
+echo "$block" | sudo tee "$FILE"
 
 # Restart process
 sudo supervisorctl reread
