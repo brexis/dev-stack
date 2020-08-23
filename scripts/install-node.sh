3df123c439585f8d313de6a2577c9cd9d4d36290
Boris Koumondji <brexis2009@yahoo.fr>
2020-08-23 12:26:50 +0000

    Update Node version

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -7,8 +7,8 @@ echo -e "export NVM_DIR=\"\$HOME/.nvm\"\n [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install 8
-nvm use 8
+nvm install 12
+nvm use 12
 
 # Install Yarn
 curl -o- -L https://yarnpkg.com/install.sh | bash
