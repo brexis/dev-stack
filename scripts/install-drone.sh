1b6ea5e869395b9dd3a4c2eefc1c12cc6f051ec1
Boris Koumondji <brexis2009@yahoo.fr>
2020-08-23 11:40:28 +0000

    Update drone installation

--- scripts/install-drone.sh
+++ scripts/install-drone.sh
@@ -1,34 +1,38 @@
 #!/usr/bin/env bash
 
-sudo docker pull drone/drone:1.0.0-rc.1
-sudo docker pull drone/agent:1.0.0-rc.1
-sudo mkdir /etc/drone
+sudo docker pull drone/drone:1
+sudo docker pull drone/drone-runner-docker:1
+sudo mkdir -p /etc/drone
 sudo touch /etc/drone/server.env
-sudo touch /etc/drone/agent.env
+sudo touch /etc/drone/runner.env
 
 docker="
 version: '3'
 
 services:
   drone-server:
-    image: drone/drone:1.0.0-rc.1
+    image: drone/drone:1
     ports:
       - 9000:80
+      - 443:443
     volumes:
-      - /var/lib/drone:/var/lib/drone
+      - /var/lib/drone:/data
     restart: always
     env_file:
       - /etc/drone/server.env
 
-  drone-agent:
-    image: drone/drone:1.0.0-rc.1
+  drone-runner:
+    image: drone/drone-runner-docker:1
+    ports:
+      - 3000:3000
+    restart: always
     depends_on:
       - drone-server
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock
     restart: always
     env_file:
-      - /etc/drone/agent.env
+      - /etc/drone/runner.env
 "
 
 echo "$docker" | sudo tee /etc/drone/docker-compose.yml
